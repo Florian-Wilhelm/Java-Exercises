b8f588fb591001888ae902ceb4312ed4ed8cc3ae
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2021-03-07 10:22:51 +0100

    Add files via upload

---
+++ Klasse1.java
@@ -0,0 +1,15 @@
+public class Klasse1 {
+   protected static int attr1; // hier besteht ein Zugriffsschutz ("protected")
+   static int attr2; // Klasselement ("static")- nicht an ein Objekt gebunden
+   int attr3; // Instanzelement - wird erst in einem Objekt verwendet
+   int A[] = {10,20,30};
+   int B = 111;
+   static void init() {
+      attr1 = 2;
+	  attr2 = 5;
+    }
+	static void ausgabe() {
+	  System.out.println(attr1);
+	  System.out.println(attr2);
+	}
+}
---
+++ KlassenSchreiben.java
@@ -0,0 +1,21 @@
+public class KlassenSchreiben {
+   public static void main(String[] args) {      
+	  System.out.println("Java-Uebung zu Variablen und Konstruktoren");
+	  Klasse1.ausgabe(); // Zugriff auf Klassenmethode
+	  Klasse1.init();
+	  Klasse1.ausgabe();
+	  Klasse1.attr1 = 42;
+	  Klasse1 ob1 = new Klasse1(); // Konsruktor ("new") - Erzeugung von ob1
+	  System.out.println(ob1.attr1);
+	  System.out.println(ob1.attr2);
+	  // die folgende Zeile ist unkluge Programmierung: Zugriff auf ein Klassenelement über eine Instanz; attr1 ist als "static" deklariert (=Klassenelement)
+	  // Klassenelemente gehören zur Klasse selbst und nicht zu einem spezifischen Objekt; grundsätzlich sollten Klassenelemente so wenig wie möglich Verwendung finden
+	  ob1.attr1 = 666; 	  
+	  System.out.println(ob1.attr1);
+	  System.out.println(ob1.attr2);
+	  System.out.println(ob1.A[1]);
+	  System.out.println(ob1.B);
+	  Klasse1.ausgabe();
+	  ob1.ausgabe();
+   }
+}
